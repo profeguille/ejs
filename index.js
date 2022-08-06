e028239804f9d075475eb2c55217d71699d2c86b
profeguille <guillermofergnani@gmail.com>
2022-08-06 04:51:29 -0300

    ejs v0

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+const server = app.listen(PORT, () => {
+  console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
+});
+
+server.on('error', (error) => console.log(`Error en servidor ${error}`));
+app.use('/public', express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set('view engine', 'ejs');
+
+let productsHC = [
+  { id: 1, title: 'nike ball', price: 101, thumbnail: 'http://localhost:8080/public/nike-ball.jpg' },
+  { id: 2, title: 'nike shoes', price: 102, thumbnail: 'http://localhost:8080/public/nike-shoes.jpg' },
+  { id: 3, title: 'adidas shoes', price: 102, thumbnail: 'http://localhost:8080/public/adidas-shoes.jpg' },
+];
+
+app.get('/products', (req, res) => {
+  console.log('aqui products');
+  res.render('pages/index', { title: 'listado de productos', products: productsHC });
+});
